3b221c6b369781d533b916febe368eaec458055c
Hollister009 <hipnos.az@gmail.com>
2019-09-04 18:35:48 +0300

    Styling of first section

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,7 @@ const types = {
   ERROR: 'view-error',
   LOADING: 'view-loading',
   INFO: 'view-info',
-}
+};
 
 /**
  * @class View
@@ -31,15 +31,15 @@ class Form extends View {
     super();
     this.message = 'Enter your location:';
     this.renderForm();
-    this.element = this.form;
+    this.element = this.form.outerHTML;
   }
 
   renderForm() {
-    this.form = document.createElement('form');
+    this.form = App.createElement('form', 'form');
 
     this.form.innerHTML = `
       <label>
-        <span>${this.message}</span>
+        <p>${this.message}</p>
         <input type="text" name="location" id="location">
         <input type="submit" value="Submit">
       </label>
@@ -79,33 +79,63 @@ class App {
     this.type = type;
     this.view = this.getCurrentView();
     this.root = document.getElementById('root');
-    this.container = this.createElement('div', 'container');
+    this.title = 'Weather App v1.0';
+    this.author = 'Andrew Zhukevych';
+    this.hero = `
+      <h1 class="title">${this.title}</h1>
+      <p class="author">by ${this.author}</p>
+    `;
 
-    this.container.appendChild(this.view.element);
-    this.root.appendChild(this.container);
+    this.createMarkup();
   }
 
   static init() {
     return new App(types.FORM);
   }
 
-  createElement(tag, className) {
+  /**
+   * @param {String} tag - tagName
+   * @param {String} [className]
+   * @returns {HTMLElement}
+   */
+  static createElement(tag, className) {
     const element = document.createElement(tag);
     if (className) element.className = className;
     return element;
+  }
+
+  static buildSection(name, content) {
+    const section = App.createElement('section', name);
+    section.innerHTML = content;
+    return section;
   }
 
   /**
    * @returns {Form|Info|ErrorMsg|Loader} instance of the view
    */
   getCurrentView() {
-    switch(this.type) {
-      case types.FORM: return new Form();
-      case types.INFO: return new Info();
-      case types.ERROR: return new ErrorMsg();
-      case types.LOADING: return new Loader();
-      default: return new ErrorMsg();
+    switch (this.type) {
+      case types.FORM:
+        return new Form();
+      case types.INFO:
+        return new Info();
+      case types.ERROR:
+        return new ErrorMsg();
+      case types.LOADING:
+        return new Loader();
+      default:
+        return new ErrorMsg();
     }
+  }
+
+  createMarkup() {
+    const container = App.createElement('div', 'container');
+    const heroSection = App.buildSection('hero', this.hero);
+    const mainSection = App.buildSection('main', this.view.element);
+    mainSection.classList.add('col-4');
+
+    container.append(...[heroSection, mainSection]);
+    this.root.appendChild(container);
   }
 }
 
